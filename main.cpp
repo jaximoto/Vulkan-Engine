abb7ce5a172d474c247fb78abb9969a8b585034c
Jaxon Simmons <62913915+jaximoto@users.noreply.github.com>
2026-03-21 09:32:12 -0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,227 @@
+﻿#define VOLK_IMPLEMENTATION
+#include <vulkan/vulkan.h>
+#include <volk/volk.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+#include <vector>
+#include <array>
+#include <string>
+#include <iostream>
+#include <fstream>
+#define VMA_IMPLEMENTATION
+#include <vma/vk_mem_alloc.h>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include "slang/slang.h"
+#include "slang/slang-com-ptr.h"
+#include <ktx.h>
+#include <ktxvulkan.h>
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
+// Globals:
+VkInstance instance{ VK_NULL_HANDLE };
+VkDevice device{ VK_NULL_HANDLE };	
+VkQueue queue{ VK_NULL_HANDLE };
+VmaAllocator allocator{ VK_NULL_HANDLE };
+VkSurfaceKHR surface{ VK_NULL_HANDLE };
+VkSwapchainKHR swapchain{ VK_NULL_HANDLE };
+bool updateSwapchain{ false };
+
+// Containers
+glm::ivec2 windowSize{};
+
+// Check Functions:
+static inline void chk(VkResult result)
+{
+	if (result != VK_SUCCESS)
+	{
+		std::cerr << "Vulkan call returned an error (" << result << ")\n";
+		exit(result);
+	}
+}
+
+static inline void chkSwapchain(VkResult result)
+{
+	if (result < VK_SUCCESS)
+	{
+		if (result == VK_ERROR_OUT_OF_DATE_KHR)
+		{
+			updateSwapchain = true;
+			return;
+		}
+		std::cerr << "Vulkan call returned an error (" << result << ")\n";
+		exit(result);
+	}
+}
+static inline void chk(bool result)
+{
+	if (!result)
+	{
+		std::cerr << "Call returned an error\n";
+		exit(result);
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	// Make Instance
+	VkApplicationInfo appInfo{
+	.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+	.pApplicationName = "New Vulkan",
+	.apiVersion = VK_API_VERSION_1_3
+	};
+
+	uint32_t instanceExtensionsCount{ 0 };
+	char const* const* instanceExtensions
+	{ SDL_Vulkan_GetInstanceExtensions(&instanceExtensionsCount) };
+
+	VkInstanceCreateInfo instanceCI{
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pApplicationInfo = &appInfo,
+		.enabledExtensionCount = instanceExtensionsCount,
+		.ppEnabledExtensionNames = instanceExtensions,
+	};
+
+	chk(vkCreateInstance(&instanceCI, nullptr, &instance));
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+
+	// Find and select device:
+	uint32_t deviceCount{ 0 };
+	chk(vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr));
+	std::vector<VkPhysicalDevice> devices(deviceCount);
+	chk(vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data()));
+
+	// pick device based off argc
+	uint32_t deviceIndex{ 0 };
+	if (argc > 1)
+	{
+		deviceIndex = std::stoi(argv[1]);
+		assert(deviceIndex < deviceCount);
+	}
+
+	// Display info of selected device
+	VkPhysicalDeviceProperties2 deviceProperties{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2 };
+	vkGetPhysicalDeviceProperties2(devices[deviceIndex], &deviceProperties);
+	std::cout << "Selected device: " << deviceProperties.properties.deviceName << "\n";
+
+	// Get Graphics queue family
+	uint32_t queueFamilyCount{ 0 };
+	vkGetPhysicalDeviceQueueFamilyProperties(devices[deviceIndex], &queueFamilyCount, nullptr);
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(devices[deviceIndex], &queueFamilyCount, queueFamilies.data());
+	uint32_t queueFamily{ 0 };
+	for (size_t i = 0; i < queueFamilies.size(); i++)
+	{
+		if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+		{
+			queueFamily = i;
+			break;
+		}
+	}
+
+	// Make sure queue supports presentation:
+	chk(SDL_Vulkan_GetPresentationSupport(instance, devices[deviceIndex], queueFamily));
+
+	// Reference queue family:
+	const float qfpriorities{ 1.0f };
+	VkDeviceQueueCreateInfo queueCI{
+		.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+		.queueFamilyIndex = queueFamily,
+		.queueCount = 1,
+		.pQueuePriorities = &qfpriorities
+	};
+
+	// Get dat swapchain:
+	const std::vector<const char*> deviceExtensions{ VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+	// Enable device features we want :)
+
+	VkPhysicalDeviceVulkan12Features enabledVk12Features{
+	.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES,
+	.descriptorIndexing = true,
+	.shaderSampledImageArrayNonUniformIndexing = true,
+	.descriptorBindingVariableDescriptorCount = true,
+	.runtimeDescriptorArray = true,
+	.bufferDeviceAddress = true
+	};
+	const VkPhysicalDeviceVulkan13Features enabledVk13Features{
+		.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES,
+		.pNext = &enabledVk12Features,
+		.synchronization2 = true,
+		.dynamicRendering = true,
+	};
+	const VkPhysicalDeviceFeatures enabledVk10Features{
+		.samplerAnisotropy = VK_TRUE
+	};
+
+	// Make the logical device
+	VkDeviceCreateInfo deviceCI{
+	.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+	.pNext = &enabledVk13Features,
+	.queueCreateInfoCount = 1,
+	.pQueueCreateInfos = &queueCI,
+	.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size()),
+	.ppEnabledExtensionNames = deviceExtensions.data(),
+	.pEnabledFeatures = &enabledVk10Features
+	};
+	chk(vkCreateDevice(devices[deviceIndex], &deviceCI, nullptr, &device));
+
+	// Get a queue to submit graphics commands to device:
+	vkGetDeviceQueue(device, queueFamily, 0, &queue);
+
+	// Setting up Vulkan Memory Allocator
+	VmaVulkanFunctions vkFunctions{
+	.vkGetInstanceProcAddr = vkGetInstanceProcAddr,
+	.vkGetDeviceProcAddr = vkGetDeviceProcAddr,
+	.vkCreateImage = vkCreateImage
+	};
+	VmaAllocatorCreateInfo allocatorCI{
+		.flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT,
+		.physicalDevice = devices[deviceIndex],
+		.device = device,
+		.pVulkanFunctions = &vkFunctions,
+		.instance = instance
+	};
+	chk(vmaCreateAllocator(&allocatorCI, &allocator));
+
+	// Make window
+	SDL_Window* window = SDL_CreateWindow("NewVulkan", 1280u, 720u,
+		SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
+	chk(SDL_Vulkan_CreateSurface(window, instance, nullptr, &surface));
+
+	// Get Surface Properties
+	VkSurfaceCapabilitiesKHR surfaceCaps{};
+	chk(vkGetPhysicalDeviceSurfaceCapabilitiesKHR(devices[deviceIndex], 
+		surface, &surfaceCaps));
+
+	// Get swapchain extent
+	// For Wayland Enjoyers:
+	VkExtent2D swapchainExtent{ surfaceCaps.currentExtent };
+	if (surfaceCaps.currentExtent.width == 0xFFFFFFFF)
+	{
+		swapchainExtent = { .width = static_cast<uint32_t>(windowSize.x), .height = static_cast<uint32_t>(windowSize.y) };
+	}
+
+	// Okay get swapchain with right extent:
+	const VkFormat imageFormat{ VK_FORMAT_B8G8R8A8_SRGB };
+	VkSwapchainCreateInfoKHR swapchainCI{
+		.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+		.surface = surface,
+		.minImageCount = surfaceCaps.minImageCount,
+		.imageFormat = imageFormat,
+		.imageColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR,
+		.imageExtent{.width = swapchainExtent.width, .height = swapchainExtent.height },
+		.imageArrayLayers = 1,
+		.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+		.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
+		.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+		.presentMode = VK_PRESENT_MODE_FIFO_KHR
+	};
+	chk(vkCreateSwapchainKHR(device, &swapchainCI, nullptr, &swapchain));
+}
